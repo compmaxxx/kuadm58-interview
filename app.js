d6c8f8a1464c8aa42fbca13549dec55915348754
compmaxxx <compmaxxx@gmail.com>
2015-03-08 17:29:28 +0700

    add service Info

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var app = angular.module('firstStation', [])
+var pathGetInfo = 'http://localhost/adm58/api/applicant?id='
+var pathPostInfo = 'http://localhost/adm58/api/applicant?id='
 
-app.controller('barcodeController', function($scope) {
-
-})
+var helper = {
+	'boolToInt': function(data) {
+		if (typeof data == 'boolean') {
+			return data ? 1 : 0
+		} else {
+			return data
+		}
+	}
+}
