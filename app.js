54cec5184dc2f77f332f053d65e7e769d0ad7ce4
compmaxxx <compmaxxx@gmail.com>
2015-03-09 09:47:45 +0700

    reset barcode

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
 var app = angular.module('station', [])
 
 var Station = {
-	1: 'STATION 1 ลงทะเบียนตรวจเอกสาร',
-	2: 'STATION 2 ตรวจสอบคุณสมบัติ',
-	3: 'STATION 3 สอบสัมภาษณ์'
+  1: 'STATION 1 ลงทะเบียนตรวจเอกสาร',
+  2: 'STATION 2 ตรวจสอบคุณสมบัติ',
+  3: 'STATION 3 สอบสัมภาษณ์'
 }
 
 var Config = {
-	// 'title': station.TITLE1,
-	'STATION': 3,
-	'PATH_GET_INFO': 'http://localhost/regis/api/applicant?id=',
-	'PATH_POST_INFO': 'http://localhost/regis/api/applicant?id='
+  'STATION': 1,
+  'PATH_GET_INFO': 'http://localhost/regis/api/applicant?id=',
+  'PATH_POST_INFO': 'http://localhost/regis/api/applicant?id='
 }
 
 // var pathGetInfo = 'http://localhost/regis/api/applicant?id='
 // var pathPostInfo = 'http://localhost/regis/api/applicant?id='
 
 var helper = {
-	'boolToInt': function(data) {
-		if (typeof data == 'boolean') {
-			return data ? 1 : 0
-		} else {
-			return data
-		}
-	}
+  'boolToInt': function(data) {
+    if (typeof data == 'boolean') {
+      return data ? 1 : 0
+    } else {
+      return data
+    }
+  }
 }
 
 app.directive('ngEnter', function() {
-	return function(scope, element, attrs) {
-		element.bind("keydown keypress", function(event) {
-			if (event.which === 13) {
-				scope.$apply(function() {
-					scope.$eval(attrs.ngEnter);
-				})
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if (event.which === 13) {
+        scope.$apply(function() {
+          scope.$eval(attrs.ngEnter);
+        })
 
-				event.preventDefault();
-			}
-		})
-	}
+        event.preventDefault();
+      }
+    })
+  }
 })
 
 // press keydown and focus
 $(document).bind('keydown', function(e) {
-	$('#barcode').focus();
+  $('#barcode').focus();
 });
--- controller.js
+++ controller.js
@@ -1,65 +1,73 @@
 app.controller('barcodeController', function($scope, infoService) {
 
-	function decode_national_id(barcode) {
-		return barcode.substr(2, 13)
-	}
+  function decode_national_id(barcode) {
+    if (barcode <= 13) {
+      return barcode.trim()
+    } else {
+      return barcode.trim().substr(2, 13)
+    }
 
-	function reset_barcode() {
-		$scope.barcode = ''
-	}
+  }
 
-	function show_modal_station1() {
-		$("#station1_modal").modal('show')
-	}
+  function reset_barcode() {
+    $scope.barcode = ''
+  }
 
-	function show_modal_station2() {
-		$("#station2_modal").modal('show')
-	}
+  function show_modal_station1() {
+    $("#station1_modal").modal('show')
+  }
 
-	function show_modal_station3() {
-		$("#station3_modal").modal('show')
-	}
-	$scope.submit = function() {
-		if (Config.STATION == 1) {
-			$scope.submit_station1()
-		} else if (Config.STATION == 2) {
-			$scope.submit_station2()
-		} else if (Config.STATION == 3) {
-			$scope.submit_station3()
-		}
-	}
+  function show_modal_station2() {
+    $("#station2_modal").modal('show')
+  }
 
-	$scope.submit_station1 = function() {
-		var national_id = decode_national_id($scope.barcode)
-		infoService.getInfo(national_id).then(function(info) {
-			$scope.info = info
-				// console.log(info)
-		})
-		show_modal_station1();
-	}
+  function show_modal_station3() {
+    $("#station3_modal").modal('show')
+  }
+  $scope.submit = function() {
+    if (Config.STATION == 1) {
+      $scope.submit_station1()
+    } else if (Config.STATION == 2) {
+      $scope.submit_station2()
+    } else if (Config.STATION == 3) {
+      $scope.submit_station3()
+    }
+  }
 
-	$scope.submit_station2 = function() {
-		var national_id = decode_national_id($scope.barcode)
-		infoService.getInfo(national_id).then(function(info) {
-			$scope.info = info
-				// console.log(info)
-		})
-		show_modal_station2();
-	}
+  $scope.submit_station1 = function() {
+    var national_id = decode_national_id($scope.barcode)
+    infoService.getInfo(national_id).then(function(info) {
+      $scope.info = info
+        // console.log(info)
+    })
+    reset_barcode()
+    show_modal_station1()
+  }
 
-	$scope.submit_station3 = function() {
-		var national_id = decode_national_id($scope.barcode)
-		infoService.getInfo(national_id).then(function(info) {
-			$scope.info = info
-				// console.log(info)
-		})
-		show_modal_station3();
-	}
+  $scope.submit_station2 = function() {
+    var national_id = decode_national_id($scope.barcode)
+    infoService.getInfo(national_id).then(function(info) {
+      $scope.info = info
+        // console.log(info)
+    })
+    reset_barcode()
+    show_modal_station2()
+  }
+
+  $scope.submit_station3 = function() {
+    var national_id = decode_national_id($scope.barcode)
+    infoService.getInfo(national_id).then(function(info) {
+      $scope.info = info
+        // console.log(info)
+    })
+    reset_barcode()
+    show_modal_station3()
+  }
 })
 
 
 app.controller('headerController', function($scope) {
-	$scope.header = {
-		'title': Station[Config.STATION]
-	}
+  $scope.header = {
+    'title': Station[Config.STATION]
+  }
 })
